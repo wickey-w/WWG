7be5ef655e46b717f7b37033674dc957d4cbd0b6
wickey <2282876205@qq.com>
2018-07-24 10:22:59 +0800

    Initial Commit

---
+++ WWG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWG
+//
+//  Created by 王世成 on 2018/7/24.
+//  Copyright © 2018年 WSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWG
+//
+//  Created by 王世成 on 2018/7/24.
+//  Copyright © 2018年 WSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
